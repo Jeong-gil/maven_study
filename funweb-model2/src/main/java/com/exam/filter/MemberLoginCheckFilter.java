0234cff11f141434585da8744e066a6dd39b9c96
YangJeongGil <bom20065@naver.com>
2021-01-21 12:52:34 +0900

    로그인 했을때만 파일 다운로드 기능 추가 ( 이미지 제외 )

--- funweb-model2/src/main/java/com/exam/filter/MemberLoginCheckFilter.java
+++ funweb-model2/src/main/java/com/exam/filter/MemberLoginCheckFilter.java
@@ -10,22 +10,28 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns = {})
+@WebFilter(urlPatterns = { "/writeForm.do", "/writePro.do", "/fileWriteForm.do", "/fileWritePro.do", "/download.do" })
 public class MemberLoginCheckFilter implements Filter {
-	
-	public void init(FilterConfig fConfig) throws ServletException {
-	}
+
+	public void init(FilterConfig fConfig) throws ServletException {}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
-		HttpServletRequest req =(HttpServletRequest) request;
+		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		
+		HttpSession session = req.getSession();
+		// 로그인 안했을때는 로그인 화면으로 리다이렉트 이동시킴
+		String id = (String) session.getAttribute("id");
+		
+		if (id == null) {
+			res.sendRedirect("/memberLogin.do");
+			return;
+		}
+		
 		chain.doFilter(request, response);
-	}
+	} // doFilter
 
-	public void destroy() {
-	}
-
+	public void destroy() {}
 }
